b5276e3980cb0c7f7183e6ebe0f064da98a2d120
Anna Grigoreva <anstepakova@yandex.ru>
2021-02-19 12:28:09 +0300

    Update prepare_venv.sh

--- prepare_venv.sh
+++ prepare_venv.sh
@@ -2,25 +2,41 @@
 
 venv_dir=venv
 venv_installed=false
+venv_activated=false
 
 if [[ ! -d "$venv_dir" ]]
 then
   echo "Create virtual environment..."
-  if ! command -v virtualenv "$venv_dir" &> /dev/null
+  if python3 -c 'import venv'
   then
-    echo "Install virtualenv before running this script!"
-  else
-    virtualenv "$venv_dir"
+    python3 -m venv "$venv_dir"
     venv_installed=true
+  else
+    echo "Install python3-venv before running this script!"
   fi
 fi
 
 if [ "$venv_installed" = true ]
 then
-  source venv/bin/activate
-  if [[ -f "requirements.txt" ]]
+  if [[ -f venv/bin/activate ]]
   then
-    echo "Install dependencies from requirements.txt..."
-    pip install -r requirements.txt
+    source venv/bin/activate
+    venv_activated=true
+  else
+    if [[ -f venv/Scripts/activate ]]
+    then
+      source venv/Scripts/activate
+      venv_activated=true
+    else
+      echo "Neither venv/bin/activate nor venv/Scripts/activate was found. Skip dependencies installation."
+    fi
+  fi
+  if [ venv_activated = true ]
+  then
+    if [[ -f "requirements.txt" ]]
+    then
+      echo "Install dependencies from requirements.txt..."
+      pip install -r requirements.txt
+    fi
   fi
 fi
