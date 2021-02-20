f3405dad33f0c1402ffc9cd5d76fca5cf39c18be
dansobolev <daniil.sob56@gmail.com>
2021-02-20 19:31:47 +0300

    Revert "delete choices"
    This reverts commit 138d968cb653e1057d66141a1130ff1ae4dc8187.

--- realestate/reviews/testimonials.py
+++ realestate/reviews/testimonials.py
@@ -2,9 +2,11 @@ from django.db import models
 
 
 class Review(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=200)
-    rating = models.IntegerField(max_length=1)
+    RATING_CHOICE = tuple((i, str(i)) for i in range(1, 6))
+
+    title = models.CharField('Название', max_length=50)
+    description = models.CharField('Описание', max_length=200)
+    rating = models.IntegerField(choices=RATING_CHOICE, default=RATING_CHOICE[0])
 
     class Meta:
         abstract = True
