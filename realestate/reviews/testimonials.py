c281e829045eb5dc3abb02b21d1926a9f87c181b
dansobolev <daniil.sob56@gmail.com>
2021-02-20 16:43:20 +0300

    refactored testiomials module

--- realestate/reviews/testimonials.py
+++ realestate/reviews/testimonials.py
@@ -2,16 +2,11 @@ from django.db import models
 
 
 class Review(models.Model):
-    class Rating(models.IntegerChoices):
-        rating1 = 1
-        rating2 = 2
-        rating3 = 3
-        rating4 = 4
-        rating5 = 5
+    RATING_CHOICE = tuple((i, str(i)) for i in range(1, 6))
 
     title = models.CharField('Название', max_length=50)
     description = models.CharField('Описание', max_length=200)
-    rating = models.IntegerField(choices=Rating.choices)
+    rating = models.IntegerField(choices=RATING_CHOICE, default=RATING_CHOICE[0])
 
     class Meta:
         abstract = True
