0f48321aae0bb0d4f2e5df66d13a76db5f4cf5cf
dansobolev <daniil.sob56@gmail.com>
2021-02-24 17:41:11 +0300

    added tests for views and renamed user profile abstract class

--- realestate/reviews/admin.py
+++ realestate/reviews/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import TenantReview, LandlordReview
 
-# Register your models here.
+admin.site.register(TenantReview)
+admin.site.register(LandlordReview)
--- realestate/users/models.py
+++ realestate/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class BaseUser(models.Model):
+class BaseUserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     lastname = models.CharField(max_length=30)
