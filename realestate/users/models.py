9e0ff0e6522e74bea023907a3251cc020d65b56b
dansobolev <daniil.sob56@gmail.com>
2021-02-27 11:55:25 +0300

    delete reduntant underscore

--- realestate/users/models.py
+++ realestate/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class BaseUserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=30)
+    firstname = models.CharField(max_length=30)
     lastname = models.CharField(max_length=30)
     middlename = models.CharField(max_length=30)
     birth_date = models.DateField(verbose_name='Date of birth')
