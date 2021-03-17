2bf1fd88a50408c2e6718c27e45f43d4a0399ae2
Anna Grigoreva <anstepakova@yandex.ru>
2021-03-17 14:32:55 +0300

    Add signal and template for rating review

--- notifications/models.py
+++ notifications/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from reviews.models import LandlordReview
+from reviews.models import LandlordReview, TenantReview
 from .views import send_email
 
 
@@ -10,6 +10,12 @@ class NotificationType(models.IntegerChoices):
     EMPTY = 0, _('Empty')
     REVIEW = 1, _('ReviewNotification')
     RATING = 2, _('TenantRatingNotification')
+
+
+notification_templates = {
+    NotificationType.REVIEW: 'review_notification.html',
+    NotificationType.RATING: 'rating_notification.html',
+}
 
 
 class NotificationContent(models.Model):
@@ -28,5 +34,17 @@ def create_notification_review(sender, instance, created, **kwargs):
             'landlord': instance.review_on.landlord.id,
             'property': instance.review_on.name
         })
-        Notification.objects.create(content=content, type=NotificationType.REVIEW)
-        send_email(content.data)
+        notification = Notification.objects.create(content=content, type=NotificationType.REVIEW)
+        send_email(content.data, notification.type)
+
+
+@receiver(post_save, sender=TenantReview)
+def create_notification_rating(sender, instance, created, **kwargs):
+    if created:
+        content = NotificationContent.objects.create(data={
+            'rated_user': instance.review_on.id,
+            'landlord': instance.reviewer.id,
+            'rating': instance.rating
+        })
+        notification = Notification.objects.create(content=content, type=NotificationType.RATING)
+        send_email(content.data, notification.type)
--- notifications/views.py
+++ notifications/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.core import mail
 
+connection = mail.get_connection()
 
-def send_email(content):
-    text = render_to_string('review_notification.html', content)
-    connection = mail.get_connection()
+
+def send_email(content, notification_type):
+    from .models import notification_templates
+    text = render_to_string(notification_templates[notification_type], content)
     connection.open()
     email = mail.EmailMessage(
         'Review',
